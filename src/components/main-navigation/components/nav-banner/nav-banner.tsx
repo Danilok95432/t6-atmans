4e0f2570596b43388d011a68299058e1942b41b2
vitmach <machihin_vitalii@mail.ru>
2025-01-23 14:52:31 +0300

    Работа с баннером шапки

--- src/components/main-navigation/components/nav-banner/nav-banner.tsx
+++ src/components/main-navigation/components/nav-banner/nav-banner.tsx
@@ -1,5 +1,14 @@
 import styles from './index.module.scss'
 
 export const NavBanner = () => {
-	return <div className={styles.navBanner}></div>
+	return (
+		<div className={styles.navBanner}>
+			<div className={styles.scene}></div>
+			<div className={styles.scene}></div>
+			<div className={styles.scene}></div>
+			<div className={styles.scene}></div>
+			<div className={styles.scene}></div>
+			<div className={styles.scene}></div>
+		</div>
+	)
 }
