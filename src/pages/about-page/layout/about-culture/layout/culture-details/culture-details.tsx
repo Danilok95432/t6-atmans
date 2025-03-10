bb6b746bf9ab780db175986b502c3ac1077bb81f
vitmach <machihin_vitalii@mail.ru>
2025-03-10 13:24:13 +0300

    fix

--- src/pages/about-page/layout/about-culture/layout/culture-details/culture-details.tsx
+++ src/pages/about-page/layout/about-culture/layout/culture-details/culture-details.tsx
@@ -13,6 +13,7 @@ export const CultureDetails = () => {
 
 	if (!cultureInfo) return <h2>Нет информации о культуре</h2>
 
+	console.log(cultureInfo)
 	return (
 		<div className={styles.cultureDetails}>
 			<h2>{cultureInfo.title}</h2>
